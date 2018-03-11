388654dfc9e78678ae836cdb6185f01fe9409fb7
JoviAndrew <jovian_andrew@hotmail.com>
2018-03-11 18:26:15 +0700

    add recursive method for 3x3 checking

--- boggle2.js
+++ boggle2.js
@@ -24,48 +24,39 @@ function findWords(){
     }
 }
 
-function findWordsInBoard(word){
-    for(let counter =0; counter<board.length; counter++){
-        for(let counter2=0; counter2<board.length; counter2++){
-            console.log(`on board : ${board[counter][counter2]} banding dengan ${word.charAt(0)}`);
-            if(word.charAt(0) === board[counter][counter2]){
-                let indexSaver = [];
-                let failedAttempt = [];
-                indexSaver.push(counter, counter2);
+function check3by3 (row, column, word, wordCount){
+    for(let count1 = column-1; count1<=column+1; count1++){
+        for(let count2 = row-1; count2<=row+1; count2++){
+            if(count1>=0 && count1<board.length && count2>=0 && count2 < board.length){
+                console.log(`on board : ${board[count1][+count2]} banding dengan ${word.charAt(wordCount)}`);
+                debugger;
                 
-                //Check kata sekeliling kata yg ditemukan dan di loop sebanyak panjang kata yg ingin dicari
-                let wordCount =0;
-
-                for(let count1 = 0; count1<3; count1++){
-                    for(let count2 = 0; count2<3; count2++){
-                        if(counter+count1>=0 && counter+count1<board.length && counter2+count2>=0 && counter2+count2 < board.length){
-                            console.log(`on board : ${board[counter+count1][counter2+count2]} banding dengan ${word.charAt(wordCount)}`);
-                            debugger;
-
-                            if(counter+count1 === failedAttempt[0] && counter2+count2 === failedAttempt[1]){
-                                continue;
-                            }else if(word.charAt(wordCount+1) === undefined){
-                                return true;
-                            }else if(word.charAt(wordCount+1) === board[counter+count1][counter2+count2]){
-                                indexSaver.push(counter+count1, counter2+count2);
-                                wordCount++;
-                                failedAttempt.push(counter+count1, counter2+count2);
-                                counter = counter-count1;
-                                counter2-=2;
-                            }else if(counter+count1 === counter+1 && column === counter2+1){
-                                counter = indexSaver[0];
-                                counter2 = indexSaver[1];
-                                wordCount =0;
-                                indexSaver = [];
-                            }
-                        }
-                    }
+                if(wordCount === word.length-1){
+                    return true;
+                }else if(word.charAt(wordCount+1) === board[count1][count2]){
+                    wordCount++;
+                    return check3by3(count1, count2, word, wordCount);
                 }
-                
             }
         }
     }
     return false;
+}
+
+function findWordsInBoard(word){
+    let exit = false;
+    for(let counter =0; counter<board.length; counter++){
+        for(let counter2=0; counter2<board.length; counter2++){
+            if(word.charAt(0) === board[counter][counter2]){
+                
+                //Check kata sekeliling kata yg ditemukan dan di loop sebanyak panjang kata yg ingin dicari
+                let wordCount = 0;
+                exit = check3by3(counter, counter2, word, wordCount);
+                                
+            }
+        }
+    }
+    return exit;
 }
 
 
